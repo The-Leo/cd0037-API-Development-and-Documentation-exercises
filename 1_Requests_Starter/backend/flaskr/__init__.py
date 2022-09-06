0929e08af3b8c7ebe4415e6cff3e8837d7668a1e
The-Leo <107513599+The-Leo@users.noreply.github.com>
2022-09-06 14:16:37 +0000

    Route that retrieves all books added

--- 1_Requests_Starter/backend/flaskr/__init__.py
+++ 1_Requests_Starter/backend/flaskr/__init__.py
@@ -14,6 +14,16 @@ BOOKS_PER_SHELF = 8
 #   - Make sure for each route that you're thinking through when to abort and with which kind of error
 #   - If you change any of the response body keys, make sure you update the frontend to correspond.
 
+def paginate_books(request, selection):
+    page =request.args.get('page', 1, type=int)
+    start = (page - 1) * BOOKS_PER_SHELF
+    end = start + BOOKS_PER_SHELF
+
+    books = [book.format() for book in selection]
+    current_books = books[start:end]
+
+    return current_books
+    
 
 def create_app(test_config=None):
     # create and configure the app
@@ -38,6 +48,22 @@ def create_app(test_config=None):
     #         update the frontend to handle additional books in the styling and pagination
     #         Response body keys: 'success', 'books' and 'total_books'
     # TEST: When completed, the webpage will display books including title, author, and rating shown as stars
+    @app.route('/books')
+    def retrieve_books():
+        selection = Book.query.order_by(Book.id).all()
+        current_books = paginate_books(request, selection)
+
+        if len(current_books) == 0:
+            abort(404)
+
+        return jsonify({
+            'success': True,
+            'books': current_books,
+            'total_books': len(Book.query.all())
+        })
+
+
+
 
     # @TODO: Write a route that will update a single book's rating.
     #         It should only be able to update the rating, not the entire representation
